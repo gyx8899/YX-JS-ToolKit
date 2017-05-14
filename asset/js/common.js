7283ddfb84599dcf9b613214541556d5b25616a9
Foresky <gyx8899@126.com>
2017-05-14 13:24:42 +0800

    [Mod] Optimize parameterArrayToItem;

--- asset/js/common.js
+++ asset/js/common.js
@@ -34,56 +34,66 @@ function loadCSS(url, callback)
 	if (!url)
 		return;
 
-	// Process the url and callback if they are array;
-	parameterArrayToItem("loadCSS", url, callback);
-
-	var link = document.createElement('link');
-	link.rel = 'stylesheet';
-	link.type = 'text/css';
-	link.href = url;
-	link.onload = function ()
+	if (Array.isArray(url))
 	{
-		callback && window[callback]();
-	};
-	link.onerror = function ()
+		// Process the url and callback if they are array;
+		parameterArrayToItem("loadCSS", url, callback);
+	}
+	else
 	{
-		console.log("Error load css:" + url);
-	};
-	document.getElementsByTagName('head')[0].appendChild(link);
+		var link = document.createElement('link');
+		link.rel = 'stylesheet';
+		link.type = 'text/css';
+		link.href = url;
+		link.onload = function ()
+		{
+			callback && window[callback]();
+		};
+		link.onerror = function ()
+		{
+			console.log("Error load css:" + url);
+		};
+		document.getElementsByTagName('head')[0].appendChild(link);
+	}
 }
 function loadScript(url, callback)
 {
 	if (!url)
 		return;
 
-	// Process the url and callback if they are array;
-	parameterArrayToItem("loadScript", url, callback);
-
-	var script = document.createElement("script");
-	script.type = "text/javascript";
-
-	if (script.readyState)
-	{  //IE
-		script.onreadystatechange = function ()
-		{
-			if (script.readyState == "loaded" ||
-					script.readyState == "complete")
-			{
-				script.onreadystatechange = null;
-				callback && window[callback]();
-			}
-		};
+	if (Array.isArray(url))
+	{
+		// Process the url and callback if they are array;
+		parameterArrayToItem("loadScript", url, callback);
 	}
 	else
-	{  //Others
-		script.onload = function ()
-		{
-			callback && window[callback]();
-		};
-	}
+	{
+		var script = document.createElement("script");
+		script.type = "text/javascript";
 
-	script.src = url;
-	document.body.appendChild(script);
+		if (script.readyState)
+		{  //IE
+			script.onreadystatechange = function ()
+			{
+				if (script.readyState == "loaded" ||
+						script.readyState == "complete")
+				{
+					script.onreadystatechange = null;
+					callback && window[callback]();
+				}
+			};
+		}
+		else
+		{  //Others
+			script.onload = function ()
+			{
+				callback && window[callback]();
+			};
+		}
+
+		script.src = url;
+		document.body.appendChild(script);
+	}
 }
 /*
  * Functions: Regular expression
@@ -98,18 +108,15 @@ function isURL(url)
 	return (new RegExp(expression)).test(url);
 }
 /*
- * Functions: Tools for processing function;
+ * Functions: Tools for processing function who has parameter array;
  * */
 function parameterArrayToItem(fn, param1, param2)
 {
-	if (Array.isArray(param1))
+	var param2IsArray = Array.isArray(param2),
+			param2ArrayLength = param2IsArray && param2.length || 0;
+	for (var i = 0, length = param1.length; i < length; i++)
 	{
-		var param2IsArray = Array.isArray(param2),
-				param2ArrayLength = param2IsArray && param2.length || 0;
-		for (var i = 0, length = param1.length; i < length; i++)
-		{
-			var param2Item = (param2IsArray && i < param2ArrayLength) ? param2[i] : null;
-			fn && window[fn](param1[i], param2Item);
-		}
+		var param2Item = (param2IsArray && i < param2ArrayLength) ? param2[i] : null;
+		fn && window[fn](param1[i], param2Item);
 	}
 }
