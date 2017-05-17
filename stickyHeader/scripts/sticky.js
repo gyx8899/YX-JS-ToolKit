ab4308f4e8d835759e8e91ac273939a386108811
Foresky <gyx8899@126.com>
2017-05-17 23:53:16 +0800

    [Add] Add demo sticky header;

---
+++ stickyHeader/scripts/sticky.js
@@ -0,0 +1,34 @@
+$(document).ready(function ()
+{
+	$(window).scroll(function ()
+	{
+		var scrollTop = $(window).scrollTop(), $header = $('.header'), headerHeight = $header.height();
+		if (scrollTop > 0)
+		{
+			setTimeout(function ()
+			{
+				if ($(window).scrollTop() >= headerHeight)
+				{
+					$header.addClass('sticky sticky-animation sticky-bg');
+				}
+				else
+				{
+					$header.addClass('sticky sticky-animation').removeClass('sticky-bg');
+				}
+				$('.header-placeholder').addClass('sticky');
+			}, 0);
+		}
+		else
+		{
+			$header.removeClass('sticky-animation').removeClass('sticky-bg');
+			setTimeout(function ()
+			{
+				if ($(window).scrollTop() == 0 && !$header.hasClass('sticky-bg'))
+				{
+					$header.removeClass('sticky-animation sticky');
+					$('.header-placeholder').removeClass('sticky');
+				}
+			}, 500);
+		}
+	});
+});
