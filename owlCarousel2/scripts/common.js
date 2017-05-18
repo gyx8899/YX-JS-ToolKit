2bcb567d481462bad9a3eb971e2e6d1701cfc41f
Foresky <gyx8899@126.com>
2017-05-19 00:45:25 +0800

    [Add] Add owlCarousel2 demos;

---
+++ owlCarousel2/scripts/common.js
@@ -0,0 +1,31 @@
+var demoBaseOwlOptions = {
+	margin: 10,
+	nav: true,
+	navText: ['', ''],
+	dots: false,
+	loop: false
+};
+function initOwlCarousel($owl, options)
+{
+	var owlOptions = $.extend({}, demoBaseOwlOptions, options);
+	return $owl.owlCarousel(owlOptions);
+}
+function owlItemClickedMonitor($owl, clickedHandler)
+{
+	$owl.on('mousedown', '.item', function ()
+	{
+		$(this).on('mouseup mousemove', function handler(event)
+		{
+			if (event.type === 'mouseup' && event.which <= 1) //only for left key
+			{
+				// Clicked (left key)
+				clickedHandler && window[clickedHandler];
+			}
+			else
+			{
+				// Dragged
+			}
+			$(this).off('mouseup mousemove', handler);
+		});
+	});
+}
