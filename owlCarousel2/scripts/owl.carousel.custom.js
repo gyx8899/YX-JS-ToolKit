8e50a8f4a5114993ba96487dfc6b920276504758
Foresky <gyx8899@126.com>
2017-08-04 19:55:30 +0800

    [Mod] Update owl.carousel.js from official site, and sync the custom js;

--- owlCarousel2/scripts/owl.carousel.custom.js
+++ owlCarousel2/scripts/owl.carousel.custom.js
@@ -1,6 +1,6 @@
 /**
- * Owl Carousel v2.2.0
- * Copyright 2013-2016 David Deutsch
+ * Owl Carousel v2.2.1
+ * Copyright 2013-2017 David Deutsch
  * Licensed under MIT (https://github.com/OwlCarousel2/OwlCarousel2/blob/master/LICENSE)
  */
 /**
