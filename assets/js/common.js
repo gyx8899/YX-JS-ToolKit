c2e2d79aedc4fdfa06d2439cc8f26e4073397a08
Foresky <gyx8899@126.com>
2017-12-13 15:02:20 +0800

    [Mod] common : Add dictionary plugin;

--- assets/js/common.js
+++ assets/js/common.js
@@ -941,3 +941,66 @@ function mouseTouchTrack(element, infoCallback)
 		}
 	}
 }
+
+//<editor-fold desc="Plugins: Common">
+/*
+* Plugins: Dictionary
+* */
+(function () {
+	"use strict";
+
+	function Dictionary()
+	{
+		this._size = 0;
+		this.datastore = Object.create(null);
+	}
+
+	Dictionary.prototype.isEmpty = function () {
+		return this._size === 0;
+	};
+
+	Dictionary.prototype.size = function () {
+		return this._size;
+	};
+
+	Dictionary.prototype.clear = function () {
+		for (var key in this.datastore)
+		{
+			delete this.datastore[key];
+		}
+		this._size = 0;
+	};
+
+	Dictionary.prototype.add = function (key, value) {
+		this.datastore[key] = value;
+		this._size++;
+	};
+
+	Dictionary.prototype.find = function (key) {
+		return this.datastore[key];
+	};
+
+	Dictionary.prototype.count = function () {
+		var n = 0;
+		for (var key in this.datastore)
+		{
+			n++;
+		}
+		return n;
+	};
+
+	Dictionary.prototype.remove = function (key) {
+		delete this.datastore[key];
+		this._size--;
+	};
+
+	Dictionary.prototype.showAll = function () {
+		for (var key in this.datastore)
+		{
+			console.log(key + "->" + this.datastore[key]);
+		}
+	};
+
+	module.exports = Dictionary;
+})();
+//</editor-fold>
