fc322a477f4cad71cb9c3300c091c260d9ce593f
Foresky <gyx8899@126.com>
2017-12-24 13:49:25 +0800

    [Mod] QUnit : Optimize test code;

--- popupDismiss/jQueryPlugin/test.js
+++ popupDismiss/jQueryPlugin/test.js
@@ -22,46 +22,60 @@
 		var sourceElement = document.querySelector('[data-target="' + targetSelector + '"]'),
 				targetElement = document.querySelector(targetSelector),
 				clickedElements = [sourceElement].concat(outerTestElements);
-		if (targetElement && targetElement.children)
-		{
-			clickedElements = clickedElements.concat(Array.prototype.slice.call(targetElement.children));
-		}
 
 		for (var i = 0, l = clickedElements.length; i < l; i++)
 		{
 			testTargetCase(sourceElement, targetElement, clickedElements[i], callback);
 		}
+
+		if (targetElement && targetElement.children)
+		{
+			for (i = 0, l = targetElement.children.length; i < l; i++)
+			{
+				testTargetCase(sourceElement, targetElement, targetElement.children[i], callback, true);
+			}
+		}
 	}
 
-	function testTargetCase(sourceElement, targetElement, clickedElement, callback)
+	function testTargetCase(sourceElement, targetElement, clickedElement, callback, isSubItem)
 	{
 		if (!isDisplay(targetElement))
 		{
 			triggerClick(sourceElement);
-		}
 
-		// Test element
-		if (isDisplay(targetElement))
-		{
-			triggerClick(clickedElement);
+			// Test element
+			if (isDisplay(targetElement))
+			{
+				triggerClick(clickedElement);
 
-			if (sourceElement !== clickedElement && clickedElement.getAttribute('data-toggle') === 'popupDismiss')
-			{
-				test(clickedElement.getAttribute('data-target'));
-			}
-			else
-			{
-				verifyPopupDismiss(targetElement, clickedElement, callback);
+				if (!!isSubItem && sourceElement !== clickedElement &&
+						((clickedElement.getAttribute('data-toggle') === 'popupDismiss') || clickedElement.querySelector('[data-toggle="popupDismiss"]')))
+				{
+					test(clickedElement.querySelector('[data-toggle="popupDismiss"]').getAttribute('data-target'), [], callback);
+				}
+				else
+				{
+					verifyPopupDismiss(sourceElement, targetElement, clickedElement, callback);
+				}
 			}
 		}
 	}
 
-	function verifyPopupDismiss(targetElement, clickedElement, callback)
+	function verifyPopupDismiss(sourceElement, targetElement, clickedElement, callback)
 	{
-		var hasDismissFalse = !!findParent(clickedElement, '[data-popup-dismiss="false"]'),
+		var isDismissScope = sourceElement.getAttribute('data-dismiss-scope'),
+				isDismiss = false,
 				isTargetDisplayed = isDisplay(targetElement),
 				clickedElementTag = clickedElement.tagName + ' ' + (clickedElement.childNodes.length === 1 ? clickedElement.innerText : clickedElement.classList);
-		callback && callback(isTargetDisplayed === !hasDismissFalse, "Element Click Passed: " + clickedElementTag);
+		if (sourceElement === clickedElement ||
+				(!!isDismissScope && (hasCloset(clickedElement, document.querySelector(isDismissScope)) || hasCloset(clickedElement, targetElement))) ||
+				!isDismissScope)
+		{
+			isDismiss = isDismissTrigger(clickedElement, targetElement);
+		}
+
+		!isDismiss && sourceElement.click();
+		callback && callback(isTargetDisplayed === !isDismiss, "Element Click Passed: " + clickedElementTag);
 	}
 
 	function triggerClick(element)
@@ -71,6 +85,26 @@
 
 	function isDisplay(element)
 	{
-		return element && element.style.display !== 'none';
+		// return element && element.style.display !== 'none';
+		return $(element).css('display') !== 'none';
+	}
+
+	function isDismissTrigger(child, parent)
+	{
+		if (hasCloset(child, parent))
+		{
+			var dataPopupDismiss = child.getAttribute('data-popup-dismiss'),
+					parentDismissFalse;
+			if (dataPopupDismiss === 'false' || dataPopupDismiss === 'true')
+			{
+				return dataPopupDismiss === 'true';
+			}
+			else if (parentDismissFalse = findParent(child, '[data-popup-dismiss="false"]'))
+			{
+				var parentDismissTrue = findParent(child, '[data-popup-dismiss="true"]');
+				return parentDismissTrue ? hasCloset(parentDismissTrue, parentDismissFalse) : false;
+			}
+		}
+		return true;
 	}
 })();
