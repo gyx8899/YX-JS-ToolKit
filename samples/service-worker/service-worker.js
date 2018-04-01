7de67958cb576fd96cb797d014c449ef154171f9
Steper <gyx8899@126.com>
2018-04-01 19:36:02 +0800

    [Mod] service-worker: format code;

--- samples/service-worker/service-worker.js
+++ samples/service-worker/service-worker.js
@@ -12,48 +12,48 @@ var PRECACHE_URLS = ['index.html', './', // Alias for index.html
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', function (event) {
-  event.waitUntil(caches.open(PRECACHE).then(function (cache) {
-    return cache.addAll(PRECACHE_URLS);
-  }).then(self.skipWaiting()));
+	event.waitUntil(caches.open(PRECACHE).then(function (cache) {
+		return cache.addAll(PRECACHE_URLS);
+	}).then(self.skipWaiting()));
 });
 
 // The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', function (event) {
-  var currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(caches.keys().then(function (cacheNames) {
-    return cacheNames.filter(function (cacheName) {
-      return !currentCaches.includes(cacheName);
-    });
-  }).then(function (cachesToDelete) {
-    return Promise.all(cachesToDelete.map(function (cacheToDelete) {
-      return caches.delete(cacheToDelete);
-    }));
-  }).then(function () {
-    return self.clients.claim();
-  }));
+	var currentCaches = [PRECACHE, RUNTIME];
+	event.waitUntil(caches.keys().then(function (cacheNames) {
+		return cacheNames.filter(function (cacheName) {
+			return !currentCaches.includes(cacheName);
+		});
+	}).then(function (cachesToDelete) {
+		return Promise.all(cachesToDelete.map(function (cacheToDelete) {
+			return caches.delete(cacheToDelete);
+		}));
+	}).then(function () {
+		return self.clients.claim();
+	}));
 });
 
 // The fetch handler serves responses for same-origin resources from a cache.
 // If no response is found, it populates the runtime cache with the response
 // from the network before returning it to the page.
 self.addEventListener('fetch', function (event) {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(caches.match(event.request).then(function (cachedResponse) {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
+	// Skip cross-origin requests, like those for Google Analytics.
+	if (event.request.url.startsWith(self.location.origin)) {
+		event.respondWith(caches.match(event.request).then(function (cachedResponse) {
+			if (cachedResponse) {
+				return cachedResponse;
+			}
 
-      return caches.open(RUNTIME).then(function (cache) {
-        return fetch(event.request).then(function (response) {
-          // Put a copy of the response in the runtime cache.
-          return cache.put(event.request, response.clone()).then(function () {
-            return response;
-          });
-        });
-      });
-    }));
-  }
+			return caches.open(RUNTIME).then(function (cache) {
+				return fetch(event.request).then(function (response) {
+					// Put a copy of the response in the runtime cache.
+					return cache.put(event.request, response.clone()).then(function () {
+						return response;
+					});
+				});
+			});
+		}));
+	}
 });
 
 //# sourceMappingURL=service-worker.js.map
