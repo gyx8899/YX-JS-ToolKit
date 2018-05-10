18d9089926dbe0a405e44ec58b559678efa2f67d
Steper <gyx8899@126.com>
2018-05-10 19:02:17 +0800

    [Mod] popupDismiss : Remove useless file;

--- popupDismiss/jsPlugin/popupDismiss-auto.js
+++
@@ -1,6 +0,0 @@
-// Auto popupDismiss for all [data-toggle="popupDismiss"] in body;
-loadScript('https://gyx8899.github.io/YX-JS-ToolKit/popupDismiss/jsPlugin/popupDismiss.js', popupDismissCallback);
-function popupDismissCallback()
-{
-	return new popupDismiss();
-}
