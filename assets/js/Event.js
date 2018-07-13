158e4d6ad98a05f8002966009999add223082039
Steper <gyx8899@126.com>
2018-07-13 18:01:14 +0800

    [Add] Event: Add class 'Event' and test page;

---
+++ assets/js/Event.js
@@ -0,0 +1,54 @@
+class Event
+{
+	constructor()
+	{
+		this._cache = {};
+	}
+	on(eventName, callback)
+	{
+		if (!this._cache[eventName])
+		{
+			this._cache[eventName] = [];
+		}
+
+		if (typeof callback === 'function' && this._cache[eventName].indexOf(callback) === -1)
+		{
+			this._cache[eventName].push(callback);
+		}
+		else
+		{
+			typeof callback !== 'function' && alert(`Your add func ${callback} is not one valid function.`);
+			this._cache[eventName].indexOf(callback) !== -1 && alert(`Same on(eventName, func) have been called!`);
+		}
+		return this;
+	}
+	off(eventName, callback)
+	{
+		let eventCallbacks = this._cache[eventName];
+		if (Array.isArray(eventCallbacks) && eventCallbacks.length)
+		{
+			if (callback)
+			{
+				eventCallbacks.splice(eventCallbacks.indexOf(callback), 1);
+			}
+			else
+			{
+				eventCallbacks.length = 0;
+			}
+		}
+		return this;
+	}
+	trigger(eventName, data)
+	{
+		let eventCallbacks = this._cache[eventName];
+		if (eventCallbacks && eventCallbacks.length)
+		{
+			eventCallbacks.forEach((func) => {
+				func(data);
+			});
+		}
+		return this;
+	}
+}
+
+export default Event;
