9f1d362de0fb01c716a0a0df2696ae046809397b
Steper <gyx8899@126.com>
2018-07-20 17:03:54 +0800

    [Mod] Remove useless compile files;

--- assets/js/jQueryPlugin.js
+++ assets/js/jQueryPlugin.js
@@ -1,14 +1,10 @@
-'use strict';
-
 (function (root, factory, plug) {
 	return factory(root.jQuery, plug);
 })(window, function ($, plug) {
 	$.fn[plug] = function () {
-		var $fields = this.find('input');
+		let $fields = this.find('input');
 		$fields.on('keyup', function () {
 			console.log('aaaaa');
 		});
-	};
+	}
 }, 'dnValidator');
-
-//# sourceMappingURL=jQueryPlugin.js.map
--- assets/js/notification.js
+++ assets/js/notification.js
@@ -1,9 +1,7 @@
-'use strict';
-
 // https://developer.mozilla.org/zh-CN/docs/Web/API/notification
 
-var title = "notification Title";
-var options = {
+let title = "notification Title";
+let options = {
 	dir: '', // 'auto', 'ltr', 'rtl'
 	lang: '', //
 	body: '', // content string
@@ -36,32 +34,36 @@ var options = {
 //
 // };
 
-var notification = new Notification(title, options);
+let notification = new Notification(title, options);
 
-function notifyMe() {
+function notifyMe()
+{
 	// 先检查浏览器是否支持
-	if (!("Notification" in window)) {
+	if (!("Notification" in window))
+	{
 		alert("This browser does not support desktop notification");
 	}
 
 	// 检查用户是否同意接受通知
-	else if (Notification.permission === "granted") {
-			// If it's okay let's create a notification
-			var _notification = new Notification("Hi there!");
-		}
+	else if (Notification.permission === "granted")
+	{
+		// If it's okay let's create a notification
+		let notification = new Notification("Hi there!");
+	}
 
-		// 否则我们需要向用户获取权限
-		else if (Notification.permission !== 'denied') {
-				Notification.requestPermission(function (permission) {
-					// 如果用户同意，就可以向他们发送通知
-					if (permission === "granted") {
-						var _notification2 = new Notification("Hi there!");
-					}
-				});
+	// 否则我们需要向用户获取权限
+	else if (Notification.permission !== 'denied')
+	{
+		Notification.requestPermission(function (permission) {
+			// 如果用户同意，就可以向他们发送通知
+			if (permission === "granted")
+			{
+				let notification = new Notification("Hi there!");
 			}
+		});
+	}
+
 
 	// 最后，如果执行到这里，说明用户已经拒绝对相关通知进行授权
 	// 出于尊重，我们不应该再打扰他们了
 }
-
-//# sourceMappingURL=notification.js.map
