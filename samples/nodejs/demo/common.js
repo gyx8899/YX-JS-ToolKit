2d21a5f09a5695e8afb8cc208839fbf824e35e62
Steper <gyx8899@126.com>
2019-08-07 18:50:02 +0800

    [Mod] add node excel2json example;

--- samples/nodejs/demo/common.js
+++ samples/nodejs/demo/common.js
@@ -41,8 +41,52 @@ function getDirectoryFolderNames(path)
 	return getDirectoryList(path).dirNames;
 }
 
+function getNodeArgs() {
+	const args = {};
+	process.argv
+			.slice(2, process.argv.length)
+			.forEach(arg => {
+				// long arg
+				if (arg.slice(0, 2) === '--') {
+					const longArg = arg.split('=');
+					const longArgFlag = longArg[0].slice(2, longArg[0].length);
+					args[longArgFlag] = longArg.length > 1 ? longArg[1] : true;
+				}
+				// flags
+				else if (arg[0] === '-') {
+					const flags = arg.slice(1, arg.length).split('');
+					flags.forEach(flag => {
+						args[flag] = true;
+					});
+				}
+			});
+	return args;
+}
+
+function getDiffFolderFileNames(sourceFolder, destFolder) {
+	let sourceFiles = getDirectoryFileNames(sourceFolder),
+			destFiles = getDirectoryFileNames(destFolder);
+	let sourceFileNames = new Set(sourceFiles.map((name) => {
+						return name.slice(0, name.lastIndexOf('.'));
+					})),
+			destFileNames = new Set(destFiles.map((name) => {
+						return name.slice(0, name.lastIndexOf('.'));
+					}));
+	let difference = new Set(
+	    [...sourceFileNames].filter(x => !destFileNames.has(x)));
+	return Array.from(difference).map(filename => {
+		for (let i = 0; i < sourceFiles.length; i++) {
+			if (sourceFiles[i].indexOf(filename) === 0) {
+				return sourceFiles[i];
+			}
+		}
+	});
+}
+
 module.exports.writeDataToFile = writeDataToFile;
 module.exports.readDataFromFile = readDataFromFile;
 module.exports.getDirectoryList = getDirectoryList;
 module.exports.getDirectoryFileNames = getDirectoryFileNames;
 module.exports.getDirectoryFolderNames = getDirectoryFolderNames;
+module.exports.getNodeArgs = getNodeArgs;
+module.exports.getDiffFolderFileNames = getDiffFolderFileNames;
--- samples/nodejs/demo/excel2json.js
+++ samples/nodejs/demo/excel2json.js
@@ -1,14 +1,13 @@
 // required
 let util = require('./common.js');
-
 let path = require("path");
+let xlsx2json = require("node-xlsx");// npm install --save-dev node-xlsx
 
-// npm install --save-dev node-xlsx
-let xlsx2json = require("node-xlsx");
+const args = util.getNodeArgs();
 
 let excelPath = path.join(__dirname, "../excel/"),
 		jsonPath = path.join(__dirname, "../json/"),
-		fileNames = util.getDirectoryFileNames(excelPath);
+		fileNames = args.name ? [args.name] : util.getDiffFolderFileNames(excelPath, jsonPath);
 
 for (let fileName of fileNames)
 {
