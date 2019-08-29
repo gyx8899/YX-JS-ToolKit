86320f41865d915e508b824239e1af8503cbd531
Steper <gyx8899@126.com>
2019-08-29 11:02:59 +0800

    [DEL] Remove useless demo;

--- samples/react/react-todo-list/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import {createLogger} from "redux-logger";
-import {createStore, applyMiddleware} from "redux";
-import todoApp from './reducers/index';
-
-const loggerMiddleware = createLogger();
-const store = createStore(todoApp, applyMiddleware(loggerMiddleware));
-
-export default store;
