7d674ce891f0bb408cc2003fd5a5b1bd3ed0fdf7
Steper <gyx8899@126.com>
2019-09-09 16:37:02 +0800

    [Mod] Update useOffline and test page;

--- samples/react/next-app-material/pages/test/_UseOffline.js
+++ samples/react/next-app-material/pages/test/_UseOffline.js
@@ -1,15 +1,32 @@
-import React, {Fragment} from "react";
+import React, {useEffect} from "react";
 import useOffline from '../hook/UseOffline';
+import {SnackbarProvider, useSnackbar} from 'notistack';
 
-function App () {
+function App() {
+	const {enqueueSnackbar} = useSnackbar();
 	const [isOffline, isReOnline] = useOffline();
 
-	return (
-			<Fragment>
-				<div>{isOffline ? '>Sorry, you are offline ...' : 'You are online!'}</div>
-				<div>{isReOnline ? 'You are Re-online ...' : ''}</div>
-			</Fragment>
-	);
+	useEffect(() => {
+		// variant could be success, error, warning, info, or default
+		if (isOffline) {
+			enqueueSnackbar(`Sorry, you are offline ...`, {variant: 'warning'});
+		}
+	}, [isOffline]);
+
+	useEffect(() => {
+		// variant could be success, error, warning, info, or default
+		if (isReOnline) {
+			enqueueSnackbar('You are Re-online ...', {variant: 'success'});
+		}
+	}, [isReOnline]);
+
+	return null;
 }
 
-export default App;
+export default function IntegrationNotistack() {
+  return (
+    <SnackbarProvider maxSnack={3}>
+      <App />
+    </SnackbarProvider>
+  );
+}
